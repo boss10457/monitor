f5be4091b7b7a040734678974df81d2e8a87dd32
boss10457 <93358229+boss10457@users.noreply.github.com>
2024-05-03 15:41:58 +0800

    Update CurlTime.sh

--- CurlTime.sh
+++ CurlTime.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-/usr/bin/curl -m 7 -o /dev/null -k -s -w  "HTTP %{http_code} DNS %{time_namelookup} CONNECT %{time_connect} SSL %{time_appconnect} Pretransfer %{time_pretransfer} Starttransfer %{time_starttransfer} TOTAL %{time_total}" https://www.google.com
+CurlData=$(/usr/bin/curl -m 7 -o /dev/null -k -s -w  "HTTP %{http_code} DNS %{time_namelookup} CONNECT %{time_connect} SSL %{time_appconnect} Pretransfer %{time_pretransfer} Starttransfer %{time_starttransfer} TOTAL %{time_total}" https://www.google.com)
+
+HttpCode=$(echo $CurlData | awk '{print $2}')
+DnsTime=$(echo $CurlData | awk '{print $4}')
+ConnectTime=$(echo $CurlData | awk '{print ($6 - $4)}')
+SslTime=$(echo $CurlData | awk '{print ($8 - $6)}')
+ServerResponseTime=$(echo $CurlData | awk '{print ($12 - $10)}')
+DataTransferTime=$(echo $CurlData | awk '{print ($14 - $12)}')
+TotalTime=$(echo $CurlData | awk '{print $14}')
+
 
 echo ''
 echo HTTP %{http_code}
